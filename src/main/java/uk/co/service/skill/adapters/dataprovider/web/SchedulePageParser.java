c7ffbc2d8c814ac530e41ba2153be6cbc53ba6d4
Darragh McConville <darragh@kainos.com>
2018-05-04 22:22:35 +0100

    Retrieved Start Date and End Date. Started to get full Schedule.

--- src/main/java/uk/co/service/skill/adapters/dataprovider/web/SchedulePageParser.java
+++ src/main/java/uk/co/service/skill/adapters/dataprovider/web/SchedulePageParser.java
@@ -2,46 +2,76 @@ package uk.co.service.skill.adapters.dataprovider.web;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import uk.co.service.skill.LoggingFacade;
 
 import org.apache.commons.lang3.StringUtils;
+import uk.co.service.skill.adapters.dataprovider.exceptions.BinCollectionGatewayException;
 
-import org.apache.commons.collections4.MultiMap;
-import org.apache.commons.collections4.map.MultiValueMap;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SchedulePageParser implements LoggingFacade {
 
-    private static final String ADDRESS_NOT_FOUND_TEXT = "No results found for the search text provided";
-    private static final String SERVICE_PROVIDER_SCHEDULE_URL_PATH = "/address";
-    private static final String DATE_DISPLAY_SELECTOR = "dateDisplay";
-    private static final String DATE_DISPLAY_SEPARATOR = "-";
+    private static final String PAGE_ENCODING = "UTF-8";
+    private static final String DISPLAY_DATE_FORMAT = "dd/MM/yyyy";
+    private static final String DISPLAY_DATE_SEPARATOR = "-";
+    private static final String DISPLAY_DATE_ID = "dateDisplay";
+    private static final String DISPLAY_ADDRESS_SELECTOR = "body > div > div:nth-child(1) > h3";
+    private static final String DISPLAY_SCHEDULE_SELECTOR = "#calendarDisplay > div.table-responsive > table > tbody";
 
-    private final String html;
-    private final Document document;
-    private  String startDate;
-    private  String endDate;
+    SimpleDateFormat dateFormatter = new SimpleDateFormat(DISPLAY_DATE_FORMAT);
+
+    private final Document scheduleWebPage;
 
     public SchedulePageParser(String html) {
-        this.html = html;
-        this.document = Jsoup.parse(html);
-        setDateRangeValues(this.document);
+        this.scheduleWebPage = Jsoup.parse(html, PAGE_ENCODING);
+        System.out.println(scheduleWebPage.select(DISPLAY_SCHEDULE_SELECTOR).toString());
     }
 
     public String getFirstLineOfAddress(){
-        return "";
+        Elements resultLinks = scheduleWebPage.select(DISPLAY_ADDRESS_SELECTOR);
+        return(resultLinks.first().text().trim());
     }
 
-    public String getStartDate(){
+    public Date getStartDate(){
+        Date startDate = null;
+        String dateRange = scheduleWebPage.getElementById(DISPLAY_DATE_ID).text();
+        String startDateString = StringUtils.substringBefore(dateRange, DISPLAY_DATE_SEPARATOR).trim();
+
+        try{
+            startDate = dateFormatter.parse(startDateString);
+        }
+        catch(ParseException pe){
+            throw new BinCollectionGatewayException("Failed to parse start date in Bin Schedule. Value - " + startDateString);
+        }
         return startDate;
     }
 
-    public String getEndDate(){
+    public Date getEndDate(){
+        Date endDate = null;
+        String dateRange = scheduleWebPage.getElementById(DISPLAY_DATE_ID).text();
+        String endDateString = StringUtils.substringBefore(dateRange, DISPLAY_DATE_SEPARATOR).trim();
+
+        try{
+            endDate = dateFormatter.parse(endDateString);}
+        catch(ParseException pe){
+            throw new BinCollectionGatewayException("Failed to parse end date in Bin Schedule. Value - " + endDateString);
+        }
         return endDate;
     }
 
     public String getSchedule() throws Exception {
 
+        // TODO - We will want to use a MultiMap to return the contents of the table.
+        // TODO - We will also want to walk the grid and assign an actual Date to each cell, or at least a proper date to a collection day.
+        // TODO - (ctd) we will also want to return the String Date e.g. 'Monday the 14th of May' which will be useful for Alexa.
+        Elements schedule = scheduleWebPage.select(DISPLAY_SCHEDULE_SELECTOR);
+
+        
+
+        System.out.println(scheduleWebPage.select(DISPLAY_SCHEDULE_SELECTOR).toString());
 //        JsonParser parser = new JsonParser();
 //        JsonObject jsonObject = parser.parse(html).getAsJsonObject();
 //        JsonElement htmlItem = jsonObject.get("html");
@@ -67,11 +97,10 @@ public class SchedulePageParser implements LoggingFacade {
         return "";
     }
 
-    private void setDateRangeValues(Document document){
-        String dateRange = document.getElementById(DATE_DISPLAY_SELECTOR).text();
-        String startDate = StringUtils.substringBefore(dateRange,DATE_DISPLAY_SEPARATOR).trim();
-        String endDate = StringUtils.substringAfter(dateRange,DATE_DISPLAY_SEPARATOR).trim();
-        this.startDate = DateUtil.formatDate(startDate); //Do we format here or outside this class?
-        this.endDate = DateUtil.formatDate(endDate);
+    public static void main (String args[]){
+
+        String selector = "body > div > div:nth-child(1) > h3";
+
+
     }
 }
